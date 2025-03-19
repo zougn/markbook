ed9f2861d954870b4b6dac7a6b58a172459960a6
zougn <1537922215@qq.com>
2025-03-20 00:17:02 +0800

    Java实现的自动提交

---
+++ markbook/src/main/java/com/zougn/markbook/MarkbookApplication.java
@@ -0,0 +1,13 @@
+package com.zougn.markbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkbookApplication.class, args);
+	}
+
+}
---
+++ markbook/src/test/java/com/zougn/markbook/MarkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zougn.markbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
